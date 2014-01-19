421cd6c74d51024615e7309b0962d1c81e11b328
Teddi96 <theodorSiAn@kvenno.is>
2014-01-19 03:40:55 +0000

    Update ffmpeg.sh
    Comments added and removed debug comments

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Made by Loki123
-# For that idiot who can't use FFMPEG
+# Created to help with FFMPEG screencast
+# Using xwininfo to locate window and record it
+# The recording will NOT follow the window
+# Just locates the window and records the position it is in
 
 uppl=$(xwininfo -frame) 
 win_size=$(echo $uppl | grep -oEe '-geometry [0-9]+x[0-9]+' | grep -oEe '[0-9]+x[0-9]+')
 win_xy=$(echo $uppl | grep -oEe "Corners: \+[0-9]+[0-9]\+[0-9]+[0-9]+" | grep -oEe "[0-9]+\+[0-9]+" | sed -e 's/+/,/' )
-#echo $win_xy
-
 
 ffmpeg -f x11grab -video_size $win_size -i :0.0+$win_xy -framerate 25 $1.mkv
 
