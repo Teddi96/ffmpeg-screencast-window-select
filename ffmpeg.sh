bcd3bcb775f42d625fd93edba8d55e3a68a85776
Teddi96 <TheodorSiAn@kvenno.is>
2014-01-19 05:02:19 +0000

    ffmpeg -name parameter added. Error if not passed

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -6,12 +6,34 @@
 # Just locates the window and records the position it is in
 
 
+### show_help #####################
+show_help () {
+	echo "Usage:"
+	echo "	$0 [Options] [filename]"
+	}
+
+while getopts "?hvwf:n:" opt; do
+	case "$opt" in
+		h|\?) show_help; exit 0;;
+		n) name=$OPTARG;;	
+	esac
+done
+
+
+### Get information about the window (placement and size) ###############
 uppl=$(xwininfo -frame) 
 win_size=$(echo $uppl | grep -oEe '-geometry [0-9]+x[0-9]+' | grep -oEe '[0-9]+x[0-9]+')
 win_xy=$(echo $uppl | grep -oEe "Corners: \+[0-9]+[0-9]\+[0-9]+[0-9]+" | grep -oEe "[0-9]+\+[0-9]+" | sed -e 's/+/,/' )
 
-## Lets start recording that window! 
-ffmpeg -f x11grab -video_size $win_size -i :0.0+$win_xy -framerate 25 $1.mkv
+
+if [ ! -n "$name" ]; then
+	echo "[ ERROR ] Parameter -n is required. You must name the video output" 
+	echo "[ HELP  ] Run $0 -? for help."
+	exit 0
+fi
+
+#Video recording
+ffmpeg -f x11grab -video_size $win_size -i :0.0+$win_xy -framerate 25 $name.mkv
 
 
 
